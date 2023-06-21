beabe6075b706fb72b49fd2ce3af722889dd873c
Harshada Wani <harshadawani@Harshadas-MacBook.local>
2023-06-22 00:10:20 +0530

    episod-02-03

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactDOM from 'react-dom/client'
 {/* <div id = 'parent'>
     <div id = 'child'>
         <h1>I am H1 tag</h1>
@@ -22,17 +23,42 @@
 
     // root.render(heading)
 
-    const heading = React.createElement('div', {id: 'parent'}, [
-        React.createElement('div', {id: 'child1'}, [
-            React.createElement('h1', {}, "I am H1 tag"),
-            React.createElement('h2', {}, 'I am in H2 tag')
-        ]),
-        React.createElement('div', {id: 'child2'}, [
-            React.createElement('h1', {}, 'I am h1 tag'),
-            React.createElement('h2', {}, 'I am h2 tag')
-        ])
-    ]);
+    // const heading = React.createElement('div', {id: 'parent'}, [
+    //     React.createElement('div', {id: 'child1'}, [
+    //         React.createElement('h1', {}, "I am H1 tag"),
+    //         React.createElement('h2', {}, 'I am in H2 tag')
+    //     ]),
+    //     React.createElement('div', {id: 'child2'}, [
+    //         React.createElement('h1', {}, 'I am h1 tag'),
+    //         React.createElement('h2', {}, 'I am h2 tag')
+    //     ])
+    // ]);
 
-    const root = ReactDOM.createRoot(document.getElementById('root'));
+    // const root = ReactDOM.createRoot(document.getElementById('root'));
 
-    root.render(heading)
+    // root.render(heading)
+
+    const elem = <span>React Element</span>
+    const title = (
+        <h1>
+        {elem},
+        This is jsx element</h1>
+    );
+    // const Title = () => (
+    //     <h1 className="head">This is title </h1>
+    // );
+
+    const HeadingComponent = () => (
+        <div>
+        {/* <Title /> */}
+        {title}
+        <h1>This is functional HeadingComponent</h1>
+        </div>
+    );
+    // const jsxHeading = (
+    // <h1 className="head" >This is JSX heading</h1>,
+    // <h2 className="head1" >This is multiple lines in jsx </h2>
+    // ) // this is react element
+    
+    const root= ReactDOM.createRoot(document.getElementById('root'))
+    root.render(<HeadingComponent />)
